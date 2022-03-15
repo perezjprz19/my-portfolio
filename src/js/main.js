4ec33c80b568e911fc66d5c49e96ce0ee9a53a8d
Jessica Shellenhamer <jessicaswebdevelopment@gmail.com>
2022-03-15 00:44:46 -0400

    Add client-side form validation to form in 'about' section with Javascript.
    * [Style] - Added styles for :valid, :invalid, :invalid:required on inputs for full name and emails.
    *[Style] - Added styles for .error and .active classes for the error messages upon invalid or incomplete entries in the 'full name' and 'email' fields of the form.
    [HTML] - Added error class to 'full name' and 'email' address inputs
    [Accessibility] - Added Aria-live = 'polite' to 'full name' and 'email' span tags used to print the errors.
    
    Changes will prevent be better conformed to accessibility guidelines which requires required fields to be advised and will allow users to get a polite notification from their screen reader if the leave a required field blank.
    
    Changes also provide visual cues that do not interfere with the design of the page.

---
+++ src/js/main.js
@@ -0,0 +1,86 @@
+//I gathered entire contact form, the email input and the span element
+//which I will use to print the error message to.
+
+const form = document.querySelector("form");
+const email = document.getElementById("email");
+const emailError = document.querySelector("#email + span.error");
+
+//Here I'm grabbing the field for the full name and it's sibling span with the class of error.
+
+const fullName = document.getElementById("fullName");
+const nameError = document.querySelector("#fullName + span.error");
+
+
+
+//Name Field Completion Validation
+fullName.addEventListener("input", function (e) {
+    if(fullName.validity.valid){
+        console.log("fullName is not blank")
+        fullName.textContent = " ";
+        fullName.class = "error";
+}else {
+        showError();
+    }
+});
+
+//I added an event listener to the email input field and in case 
+//Email Field Validation
+email.addEventListener("input", function (e) {
+//the form is showing an error message for the email when it is in fact valid,
+
+    if (email.validity.valid){
+
+//I will remove the error message by setting the span with the class error to 
+        emailError.textContent = " ";
+        emailError.className = "error";
+
+
+}else {
+
+    //an empty string. Otherwise, if the email is invalid, it will show an error.
+        showError();
+    }
+});
+
+
+//Here I added an event listener to the form that says
+form.addEventListener("submit", function (e) {
+// if the email address  or full name field is not valid
+    if (!email.validity.valid || !fullName.validity.valid){
+//show an error
+        showError();
+//and prevent the form from being submitted.
+        e.preventDefault();
+    }
+});
+
+//Here the error function is declared.
+function showError() {
+    
+    //if the full name field is empty, print the specified error.
+    if (fullName.validity.valueMissing) {
+       
+        nameError.textContent = "This field cannot be blank."
+         //if the email field is empty 
+
+    }else if(email.validity.valueMissing) {
+
+        //show the specified error below.
+        emailError.textContent = "This field cannot be blank";
+
+        //Otherwise if the input in the email field does not match the expected format (x@y)
+    } else if(email.validity.typeMismatch) {
+
+//show the error specified below:
+        emailError.textContent = "Email is not in proper format.";
+    }
+
+//This is setting the class names for styling purposes.
+    emailError.className = "error active";
+    nameError.className = "error active";
+};
+
+
+
+
+
